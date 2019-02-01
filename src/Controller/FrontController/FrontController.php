ee65060439995982b0e2fed9dd12afe813baca26
Ludovic BLONDON <ludovicblondon@gmail.com>
2019-02-01 11:55:51 +0100

    feat: frontend

---
+++ src/Controller/FrontController/FrontController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller\FrontController;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @var PostRepository
+     */
+    private $repository;
+
+    /**
+     * FrontController constructor.
+     * @param PostRepository $repository
+     */
+    public function __construct(PostRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
+    public function home(): Response
+    {
+        $posts = $this->repository->findLatest();
+        return $this->render('front/home.html.twig', [
+            'current_menu' => 'home',
+            'posts' => $posts
+        ]);
+
+    }
+
+    /**
+     * @Route("/toutes-les-news", name="allNews")
+     * @return Response
+     */
+    public function allNews(): Response
+    {
+        $posts = $this->repository->findAll();
+        return $this->render('front/allnews.html.twig', [
+            'current_menu' => 'allnews',
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/article/{slug}-{id}", name="post.show", requirements={"slug": "[a-z0-9\-]*"})
+     * @return Response
+     */
+    public function show(Post $post, string $slug)
+    {
+        if ($post->getSlug() !== $slug){
+            return $this->redirectToRoute('post.show', [
+                'id' => $post->getId(),
+                'slug' => $post->getSlug()
+            ], 301);
+        }
+        return $this->render('front/show.html.twig', [
+            'current_menu' => 'allnews',
+            'post' => $post
+        ]);
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $post_at;
+
+    public function __construct()
+    {
+        $this->post_at = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return (new Slugify())->slugify($this->title);
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPostAt(): ?\DateTimeInterface
+    {
+        return $this->post_at;
+    }
+
+    public function setPostAt(\DateTimeInterface $post_at): self
+    {
+        $this->post_at = $post_at;
+
+        return $this;
+    }
+}
